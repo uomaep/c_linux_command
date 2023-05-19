0331347f963bf8de8143a647549e8d07dd6b3f10
Daewon Lim <114221785+uomaep@users.noreply.github.com>
2023-05-19 19:10:27 +0900

    Add files via upload

---
+++ date_tmp.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <time.h>
+
+char* today(struct tm* t)
+{
+    switch(t->tm_wday) {
+        case 0:
+            return "월";
+        case 1:
+            return "화";
+        case 2:
+            return "수";
+        case 3:
+            return "목";
+        case 4:
+            return "금";
+        case 5:
+            return "토";
+        case 6:
+            return "일";
+        default:
+            return "";
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    struct tm* t;
+    time_t base = time(NULL);
+
+    t = localtime(&base);
+
+    printf("%d년 %d월 %d일 %s요일 %d시 %d분 %d초\n", 1900 + t->tm_year, t->tm_mon + 1, t->tm_mday + 1, today(t), t->tm_hour, t->tm_min, t->tm_sec);
+    
+    return 0;
+}
