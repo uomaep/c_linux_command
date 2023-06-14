b5b3bbadd3587b134ee10372c748a06e074347ad
Daewon Lim <114221785+uomaep@users.noreply.github.com>
2023-06-14 13:18:42 +0900

    Add files via upload

--- clear_tmp.c
+++ clear_tmp.c
@@ -1,6 +1,4 @@
-#include <stdio.h>
 #include <unistd.h>
-
 int main(int argc, char* argv[]) {
     write(1, "\33[H\33[J", 6); // 이스케이프 시퀀스: 터미널에 특정 명령을 전달하기 위해 사용되는 제어 문자열
                                // \33 : 이스케이프 문자를 나타내는 ASCII 코드
